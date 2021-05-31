5d62a662937846e922dd928426dc85b320c075ba
padmavathi694 <85109878+padmavathi694@users.noreply.github.com>
2021-05-31 17:51:34 +0530

    Add files via upload

---
+++ project7.js
@@ -0,0 +1,17 @@
+//the value of variable is the age of a person
+const myAge=20;
+//the value of variable is 2 it is changed
+let earlyYears=2;
+ earlyYears=earlyYears*10.5;
+//the value of varible is changed later
+let laterYears=myAge-2;
+//the value of variable is multiplied by 4
+ laterYears=laterYears*4;
+//console.log(earlyYears);
+//console.log(laterYears);
+
+//add E.Y and L.Y 
+let myAgeInDogYears=earlyYears+laterYears;
+//print the name as a lowercase using toLowerCase()
+let myName="PADMA".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
